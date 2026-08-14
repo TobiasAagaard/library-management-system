21dffba7cea9b85c5e522af7e1db6699fe49f51a
TobiasAagaard <tach@trifork.com>
2026-08-14 15:06:52 +0200

    Implement initial MainViewModel with library data and update MainWindow layout

--- gui/Lms.Desktop/ViewModels/MainViewModel.cs
+++ gui/Lms.Desktop/ViewModels/MainViewModel.cs
@@ -1,9 +1,29 @@
+using System.Collections.Generic;
 using CommunityToolkit.Mvvm.ComponentModel;
+using Lms.Domain.Models;
 
 namespace Lms.Desktop.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+    private readonly Library _library;
+
+    public MainViewModel()
+    {
+        _library = new Library
+        {
+            Id = 1,
+            Name = "Tech Colleage Library",
+            Books = new List<Book>
+            {
+                new Book { Id = 1, Title = "Book 1", Author = "Author 1" },
+                new Book { Id = 2, Title = "Book 2", Author = "Author 2" },
+                new Book { Id = 3, Title = "Book 3", Author = "Author 3" }
+            }
+        };
+    }
+
+    public string LibraryName => _library.Name;
+
+    public int BookCount => _library.Books.Count;
 }
