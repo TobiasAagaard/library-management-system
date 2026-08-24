e9cbd338f29a128a5fc1b6cdd0ceb8bdfdb8ef71
TobiasAagaard <tach@trifork.com>
2026-08-24 13:35:55 +0200

    Add ILibraryRepository interface and LibraryRepository implementation

---
+++ libs/Lms.Application/Repositories/ILibraryRepository.cs
@@ -0,0 +1,9 @@
+using Lms.Application.Models;
+namespace Lms.Application.Repositories;
+
+public interface ILibraryRepository
+{
+    Task<IReadOnlyList<Library>> GetAllLibrariesAsync();
+    Task<Library> GetLibraryByIdAsync(Guid libraryId);
+    Task<Library> CreateLibraryAsync(Library library);
+} 
---
+++ libs/Lms.Application/Repositories/LibraryRepository.cs
@@ -0,0 +1,30 @@
+using Lms.Application.Models;
+
+namespace Lms.Application.Repositories;
+
+public class LibraryRepository : ILibraryRepository
+{
+    private readonly List<Library> _libraries = new List<Library>();
+
+    public async Task<IReadOnlyList<Library>> GetAllLibrariesAsync()
+    {
+        return await Task.FromResult(_libraries.AsReadOnly());
+    }
+
+    public async Task<Library> GetLibraryByIdAsync(Guid libraryId)
+    {
+        var library = _libraries.FirstOrDefault(l => l.Id == libraryId);
+        if (library == null)
+        {
+            throw new KeyNotFoundException($"Library with ID {libraryId} not found.");
+        }
+        return await Task.FromResult(library);
+    }
+
+    public async Task<Library> CreateLibraryAsync(Library library)
+    {
+        library.Id = Guid.NewGuid();
+        _libraries.Add(library);
+        return await Task.FromResult(library);
+    }
+}
